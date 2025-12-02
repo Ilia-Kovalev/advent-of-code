56330e8ddea26874d5ff9cfd41d7eb25c337fe66
Ilia Kovalev <kovalevis321@gmail.com>
2025-12-02 21:00:27 +0100

    Day 2, part two

--- 2025/2_solution.py
+++ 2025/2_solution.py
@@ -1,11 +1,21 @@
+import re
+
 INPUT_FILE = '2025/2_input.txt'
 
 
-def find_answer(line: str):
+def find_answer(line: str, i_part: int):
     answer = 0
     
     ranges = line.split(',')
     
+    match i_part:
+        case 1:
+            matcher = re.compile(r'((^\d+)\2$)')
+        case 2:
+            matcher = re.compile(r'((^\d+)\2+$)')
+        case _:
+            raise ValueError(f'Unexpected part {i_part}')
+
     for r in ranges:
         b, e = r.split('-')
         b = int(b)
@@ -13,15 +23,7 @@ def find_answer(line: str):
         
         invalid_ids = []
         for id in range(b, e + 1):
-            id_str = str(id)
-            n_digits = len(id_str)
-
-            if n_digits % 2 != 0:
-                continue
-            
-            i_middle = int(n_digits / 2)
-            
-            if id_str[:i_middle] == id_str[i_middle:]:
+            if matcher.match(str(id)):
                 invalid_ids.append(id)
 
         if invalid_ids:
@@ -34,4 +36,4 @@ def find_answer(line: str):
     print(f'Adding up all the invalid IDs in this example produces {answer}.')
     
 with open(INPUT_FILE) as f:
-    find_answer(f.readline())
+    find_answer(f.readline(), 2)
