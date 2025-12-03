4a3aa89e77de949999a0f1680931e84c7ea08f05
Ilia Kovalev <kovalevis321@gmail.com>
2025-12-03 20:25:39 +0100

    Day 3, part two

--- 2025/3_solution.py
+++ 2025/3_solution.py
@@ -3,19 +3,24 @@ from typing import Iterable
 INPUT_FILE = '2025/3_input.txt'
 
 
-def find_answer(banks: Iterable[str]):
+def find_answer(banks: Iterable[str], n_batteries: int):
     answer = 0
     
     for bank in banks:
         bank = bank.strip()
-        first_digit = max(bank[:-1])
-        second_digit = max(bank[bank.index(first_digit) + 1:])  
-        max_joltage = 10 * int(first_digit) + int(second_digit)
-        answer += max_joltage
+        max_joltage = 0
+        i_last = -1
 
+        for i in range(n_batteries):
+            current_slice = bank[i_last + 1: len(bank) - (n_batteries - i) + 1]
+            battary_joltage = max(current_slice)
+            i_last = current_slice.index(battary_joltage) + i_last + 1
+            max_joltage += 10 ** (n_batteries - i - 1) * int(battary_joltage)
+
+        answer += max_joltage
         print(f'In {bank} max joltage is {max_joltage}')
-        
+
     print(f'Total joltage is {answer}')
 
 with open(INPUT_FILE) as f:
-    find_answer(f)
+    find_answer(f, 12)
