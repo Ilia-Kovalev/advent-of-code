869300a993e0059bf9ebebae1741d0a49b8d27a4
Ilia Kovalev <kovalevis321@gmail.com>
2025-12-05 10:21:20 +0100

    Day 4, part two

--- 2025/4_solution.py
+++ 2025/4_solution.py
@@ -3,8 +3,8 @@ from typing import Iterable
 INPUT_FILE = '2025/4_input.txt'
 
 
-def find_answer(diagram: Iterable[str], availability_threshold: int):
-    grid = tuple(l.strip() for l in diagram)
+def find_answer(diagram: Iterable[str], availability_threshold: int, can_remove=False):
+    grid = [l.strip() for l in diagram]
     n_rows = len(grid)
     n_cols = len(grid[0])
     
@@ -27,11 +27,23 @@ def find_answer(diagram: Iterable[str], availability_threshold: int):
         (1, 1)
     )
     
-    for i in range(n_rows):
-        for j in range(n_cols):
-            if grid[i][j] == '@':
-                answer += sum(is_occupied(i+x, j+y) for x, y in directions) < availability_threshold
-    
+    roll_removed = True
+
+    while roll_removed:
+        roll_removed = False
+
+        for i in range(n_rows):
+            for j in range(n_cols):
+                if grid[i][j] == '@':
+                    accessible = sum(is_occupied(i+x, j+y)
+                                     for x, y in directions) < availability_threshold
+
+                    if accessible and can_remove:
+                        grid[i] = grid[i][:j] + '.' + grid[i][j + 1:]
+                        roll_removed = True
+
+                    answer += accessible
+
     print(f'Answer is {answer}')
 
 
@@ -50,6 +62,6 @@ EXAMPLE = '''
 
 if True:
     with open(INPUT_FILE) as f:
-        find_answer(f, 4)
+        find_answer(f, 4, True)
 else:
-    find_answer(EXAMPLE, 4)
+    find_answer(EXAMPLE, 4, True)
