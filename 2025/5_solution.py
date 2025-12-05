497ff317bbde12dd49b49eda6c0ca2876d08c80a
Ilia Kovalev <kovalevis321@gmail.com>
2025-12-05 19:56:23 +0100

    Day 5, part two

--- 2025/5_solution.py
+++ 2025/5_solution.py
@@ -3,15 +3,48 @@ from typing import Iterable
 INPUT_FILE = '2025/5_input.txt'
 
 
-def find_answer(lines: Iterable[str]):    
+def find_answer(lines: Iterable[str]):
     fresh_id_ranges = []
- 
+    answer2 = 0
+
     for l in lines:
         l = l.strip()
         if not l:
             break
 
-        fresh_id_ranges.append(tuple(map(int, l.split('-'))))
+        cur_range = list(map(int, l.split('-')))
+
+        i_ranges_to_remove = []
+        for i, range in enumerate(fresh_id_ranges):
+            if cur_range[0] > range[1] or cur_range[1] < range[0]:
+                continue
+
+            if cur_range[0] >= range[0] and cur_range[1] <= range[1]:
+                cur_range = None
+                break
+
+            if cur_range[0] <= range[0] and cur_range[1] >= range[1]:
+                i_ranges_to_remove.append(i)
+                continue
+
+            if cur_range[0] <= range[0] <= cur_range[1] <= range[1]:
+                cur_range[1] = range[1]
+                i_ranges_to_remove.append(i)
+                continue
+
+            if range[0] <= cur_range[0] <= range[1] <= cur_range[1]:
+                cur_range[0] = range[0]
+                i_ranges_to_remove.append(i)
+                continue
+
+        for i in reversed(i_ranges_to_remove):
+            del fresh_id_ranges[i]
+
+        if cur_range:
+            fresh_id_ranges.append(cur_range)
+
+    for b, e in fresh_id_ranges:
+        answer2 += e - b + 1
 
     answer = 0
     for id in lines:
@@ -23,6 +56,7 @@ def find_answer(lines: Iterable[str]):
                 break
 
     print(f'Answer is {answer}')
+    print(f'Answer for part 2 is {answer2}')
 
 
 EXAMPLE = iter('''
