5ce69b84b5cf19bba1526d3c47050323e9a300bf
Ilia Kovalev <kovalevis321@gmail.com>
2025-12-07 11:36:18 +0100

    Day 7, part two

--- 2025/7_solution.py
+++ 2025/7_solution.py
@@ -7,22 +7,40 @@ def find_answer(lines: Iterable[str]):
     first_line = next(lines)
 
     n_splits = 0
-    beam_poss = set([first_line.index('S')])
+    beam_poss = {first_line.index('S'): 1}
 
     for line in lines:
-        new_beam_poss = set()
-
-        for pos in beam_poss:
+        new_beam_poss = {}
+        for pos, n_timelines in beam_poss.items():
             match line[pos]:
                 case '.':
-                    new_beam_poss.add(pos)
+                    new_poss = [pos]
                 case '^':
-                    new_beam_poss.update([pos - 1, pos + 1])
+                    new_poss = pos - 1, pos + 1
                     n_splits += 1
+
+            for new_pos in new_poss:
+                if new_pos in new_beam_poss:
+                    new_beam_poss[new_pos] += n_timelines
+                else:
+                    new_beam_poss[new_pos] = n_timelines
 
         beam_poss = new_beam_poss
 
+        vals = tuple(map(str, beam_poss.values()))
+        max_val_len = max(map(len, vals))
+        vals = [v.zfill(max_val_len) for v in vals]
+        message = [line] * max_val_len
+        for pos, val in zip(beam_poss, vals):
+            for i, c in enumerate(val):
+                m = message[i]
+                message[i] = m[:pos] + c + m[pos+1:]
+
+        print('\n'.join(message), end=': ')
+        print(sum(beam_poss.values()))
+
     print(f'Answer is {n_splits}')
+    print(f'Answer 2 is {sum(beam_poss.values())}')
 
 
 EXAMPLE = iter('''
