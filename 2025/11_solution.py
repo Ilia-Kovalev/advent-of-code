f56f7783949aed5044bb9697167e74137951c416
Ilia Kovalev <kovalevis321@gmail.com>
2025-12-11 15:08:06 +0100

    Day 11, part two

--- 2025/11_solution.py
+++ 2025/11_solution.py
@@ -1,12 +1,10 @@
-from typing import Iterable
+from typing import Iterable, List
 import networkit as nk
 
 INPUT_FILE = '2025/11_input.txt'
 
 
-def find_answer(lines: Iterable[str]):
-    answer = 0
-    
+def find_answer(lines: Iterable[str], src: str, dest: str, required: List[str] = None):
     g = nk.Graph(directed=True)
     
     node_mapper = {}
@@ -22,15 +20,22 @@ def find_answer(lines: Iterable[str]):
         for o in outputs:
             g.addEdge(node_mapper[node], node_mapper[o])
     
-        
-    answer = nk.reachability.AllSimplePaths(
-        g, node_mapper['you'], node_mapper['out']).run().numberOfSimplePaths()
+    if required:
+        answer = 1
+        cur_src = src
+        for req in required + [dest]:
+            answer *= nk.reachability.AllSimplePaths(
+                g, node_mapper[cur_src], node_mapper[req]).run().numberOfSimplePaths()
+            cur_src = req
+    else:
+        answer = nk.reachability.AllSimplePaths(
+            g, node_mapper[src], node_mapper[dest]).run().numberOfSimplePaths()
+
 
     print(f"Answer is {answer}")
 
 
-
-EXAMPLE = iter('''
+EXAMPLE1 = iter('''
 aaa: you hhh
 you: bbb ccc
 bbb: ddd eee
@@ -43,9 +48,25 @@ hhh: ccc fff iii
 iii: out
 '''[1:-1].split('\n'))
 
+EXAMPLE2 = iter('''
+svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out
+'''[1:-1].split('\n'))
+
 
 if True:
     with open(INPUT_FILE) as f:
-        find_answer(f)
+        find_answer(f, 'svr', 'out', ['fft', 'dac'])
 else:
-    find_answer(EXAMPLE)
+    find_answer(EXAMPLE2, 'svr', 'out', ['fft', 'dac'])
